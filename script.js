aae9e0f7ba9b14c5f9da1ac190874304261750aa
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-03-08 18:55:18 +0100

    Make the delete button interactive

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+const groceryContianer =   document.querySelector('.grocery-container');
+
 function addGrocery(){
   let groceryName = document.getElementById('grocery-name').value;
-  console.log(groceryName);
 
   document.querySelector('.grocery-container').innerHTML += `
         <div class="grocery-list">
           <h3>${groceryName}</h3>
           <div class="icons">
             <span>✔</span>
-            <span><img src="download-1.png" class="delete" alt="" /></span>
+            <span><img src="download-1.png" class="delete" alt="" onclick = "deleteGrocery();" /></span>
           </div>
         </div>
   `;
+}
+
+function deleteGrocery(){
+  groceryContianer.addEventListener('click', ((e) =>{
+    if(e.target.tagName === 'IMG'){
+      e.target.parentElement.parentElement.parentElement.remove();
+    }
+  }))
 }
 
 document.getElementById('add').addEventListener('click', addGrocery);
