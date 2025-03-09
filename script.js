c29215ceeb47a967ff771c1a66287a20e1f401da
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-03-09 13:26:29 +0100

    Get the current date  to any newly created gorccery

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ const groceryContianer =   document.querySelector('.grocery-container');
 
 function addGrocery(){
   let groceryName = document.getElementById('grocery-name').value;
+  const now = new Date;
+  const date = now.getDate();
+  const month = now.getMonth();
+  const year = now.getFullYear();
 
-  document.querySelector('.grocery-container').innerHTML += `
+  groceryContianer.innerHTML += `
         <div class="grocery-list">
           <h3>${groceryName}</h3>
+          <h3>${date}/${ month}/${year}</h3>
           <div class="icons">
             <span>✔</span>
-            <span><img src="download-1.png" class="delete" alt="" onclick = "deleteGrocery();" /></span>
+            <span><img src="download-1.png" class="delete" alt="" /></span>
           </div>
         </div>
   `;
 }
 
-function deleteGrocery(){
   groceryContianer.addEventListener('click', ((e) =>{
     if(e.target.tagName === 'IMG'){
       e.target.parentElement.parentElement.parentElement.remove();
     }
-  }))
-}
+  }));
 
 document.getElementById('add').addEventListener('click', addGrocery);
