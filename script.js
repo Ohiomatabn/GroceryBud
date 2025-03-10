93c30b6de633ab8755edef0d0963ed90b08f622b
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-03-10 14:03:03 +0100

    Creat the saveToLocalStorage function

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+//get the grocery-container and assign it to gorceryConttainer
 const groceryContianer =   document.querySelector('.grocery-container');
+//Aftter the window is full load set whatever is in local storage as inner html of gorceryContainer, if null set it to and empty string 
+window.onload = groceryContianer.innerHTML = localStorage.getItem('Grocery List') || '';
 
+
+//Declare the addGrocery function
 function addGrocery(){
   let groceryName = document.getElementById('grocery-name').value;
+  //get the current Date
   const now = new Date;
+  //Extract the date, month, and year of the new Date method
   const date = now.getDate();
   const month = now.getMonth();
   const year = now.getFullYear();
 
+  //Add this to groceryContainer as innerHTML
   groceryContianer.innerHTML += `
         <div class="grocery-list">
           <h3>${groceryName}</h3>
@@ -17,16 +25,34 @@ function addGrocery(){
           </div>
         </div>
   `;
+    //Call saveToLocalStorage Function
+  saveToLocalStorage();
 }
 
-  groceryContianer.addEventListener('click', ((e) =>{
+//Add event listener to the grocery container
+groceryContianer.addEventListener('click', ((e) =>{
+  //If the tag name of the click area is groceryContainer execute this code
     if(e.target.tagName === 'IMG'){
+      //If the condition is true then remove the grand parent of this element
       e.target.parentElement.parentElement.parentElement.remove();
+    //Call saveToLocalStorage Function
+      saveToLocalStorage();
     }
   }));
 
-  document.getElementById('clear').addEventListener('click', () =>{
+  //Add event listener to the clear class
+document.getElementById('clear').addEventListener('click', () =>{
+  // Change all the innerHTML of grocery container to an empty string
     groceryContianer.innerHTML = '';
-  })
+    //Call saveToLocalStorage Function
+    saveToLocalStorage();
+});
 
+//Declare saveToLocalStorage function
+function saveToLocalStorage(){
+  //Save the innerHTML of groceryContainer as Grocery List in localStorage
+  localStorage.setItem('Grocery List', groceryContianer.innerHTML);
+}
+
+//Add event listner to the add button
 document.getElementById('add').addEventListener('click', addGrocery);
